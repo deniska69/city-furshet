64aff327a718f4a05836d4147b7ddbdf96e41704
deniska69 <gostev.dy@yandex.ru>
2024-06-20 01:19:09 +0700

    add custom css framework, enable monserate font, add favicon

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include 'env.php'
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <title><?= $TITLE ?>"</title>
+    <title><?= $TITLE ?></title>
 </head>
 
 <body>
