6cc43b983e1e776533c5778f207c999d8431652f
deniska69 <gostev.dy@yandex.ru>
2024-06-22 21:17:11 +0700

    add slider

--- index.php
+++ index.php
@@ -9,11 +9,12 @@ include 'env.php';
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, viewport-fit=cover, initial-scale=1, maximum-scale=1, user-scalable=0, interactive-widget=resizes-content" />
-    <link rel="stylesheet" href="<?= $DIR_CSS ?>/_framework.css?ver=1" />
-    <link rel="stylesheet" href="<?= $DIR_CSS ?>/_style.css?ver=1" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="<?= $DIR_CSS ?>/_framework.css?ver=1" />
+    <link rel="stylesheet" href="<?= $DIR_CSS ?>/_style.css?ver=1" />
     <title><?= $TITLE ?></title>
 </head>
 
@@ -24,7 +25,7 @@ include 'env.php';
         <!-- Header: Desktop -->
         <div id="header-menu_desktop">
             <a href="">Меню</a>
-            <a href="">Акции</a>
+            <a href="#block_2">Акции</a>
             <a href="">Доставка и оплата</a>
             <a href="">Контакты</a>
             <a href="" id="header-basket-wrap">
@@ -69,10 +70,24 @@ include 'env.php';
     </div>
 
     <!-- Block #2 - Promotions -->
-    <div id="block_2" class="noselect"></div>
+    <div id="block_2" class="noselect">
+        <!-- Swiper -->
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"></div>
+                <div class="swiper-slide"></div>
+                <div class="swiper-slide"></div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
 
     <!-- Scripts -->
     <script src="<?= $DIR_JS ?>/header.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="<?= $DIR_JS ?>/swiper.js"></script>
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
     <!-- <script src="send.js"></script> -->
 </body>
