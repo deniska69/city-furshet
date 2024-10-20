2c06ee4680009c3db54f75dabd0edd18a186cbbf
deniska69 <gostev.dy@yandex.ru>
2024-10-21 02:27:09 +0700

    test send and deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import $ from "jquery";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
+
+  const onPress = () => {
+    console.log(window.location.href);
+
+    $.ajax({
+      type: "POST",
+      url: window.location.href + "send.php",
+      data: { text: "Test message!" },
+      success: function (data) {
+        // setResult(data);
+        console.log("success:");
+        console.log(data);
+      },
+      error: function (error) {
+        console.log("error:");
+        console.error(error);
+      },
+    });
+  };
 
   return (
     <>
@@ -18,18 +38,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> */}
+        <button onClick={onPress}>send</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
