59f059058fdb9bf03e4d2ac7ee43b100f640b62c
deniska69 <gostev.dy@yandex.ru>
2024-10-22 04:14:35 +0700

    complete home block

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,20 @@
 import "./Home.css";
 
+import logo from "assets/home/logo-color-500w.png";
+
 const Home = () => {
   return (
-    <div id="home">
-      <div id="home-container"></div>
+    <div id="home" className="noselect">
+      <div id="home-container">
+        <img id="home-container-logo" src={logo} alt="logo_color_500w" />
+        <div id="home-container-description_first-wrap">
+          <span id="home-container-description_first">Вкуснее, чем дома</span>
+        </div>
+        <span id="home-container-description_second">Быстрее, чем у плиты</span>
+        <a id="home-container-button" href="#menu">
+          Посмотреть меню
+        </a>
+      </div>
     </div>
   );
 };
