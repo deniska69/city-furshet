b25c09e541af78b4d27a4d7e46ff7f291f67354e
deniska69 <gostev.dy@yandex.ru>
2024-10-30 05:50:40 +0700

    add icon fabric component

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import Categories from "./Categories";
 import Dialog from "./Dialog";
 import HeaderDesktop from "./HeaderDesktop";
 import HeaderMobile from "./HeaderMobile";
+import Icon from "./Icon";
 import Loader from "./Loader";
 
-export { Card, CardBasket, Categories, Dialog, HeaderDesktop, HeaderMobile, Loader };
+export { Card, CardBasket, Categories, Dialog, HeaderDesktop, HeaderMobile, Icon, Loader };
