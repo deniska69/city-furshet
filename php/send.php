e64bf712760c6a2cbf7aedc7c9e172772836ef2d
deniska69 <gostev.dy@yandex.ru>
2024-11-04 17:31:57 +0700

    upd

--- php/send.php
+++ php/send.php
@@ -4,7 +4,4 @@ header('Access-Control-Allow-Origin: *');
 
 require_once 'wp-config.php';
 
-$TOKEN_TELEGRAM = TOKEN_TELEGRAM;
-$CHAT_ID = CHAT_ID;
-
-fopen("https://api.telegram.org/bot{$TOKEN_TELEGRAM}/sendMessage?chat_id={$CHAT_ID}&parse_mode=html&text={$_POST['message']}", "r");
+sendToTelegram($_POST['message']);
