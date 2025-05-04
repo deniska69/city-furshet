e252244666d9a37fdaf55292c44349b171e0375b
deniska69 <gostev.dy@yandex.ru>
2025-05-04 13:03:30 +0700

    display of menu items in the mobile version only for the selected category 🍗

--- src/containers/MenuSections.tsx
+++ src/containers/MenuSections.tsx
@@ -1,6 +1,5 @@
 import { observer } from 'mobx-react';
 
-import { useWindowDimensions } from '@hooks';
 import { priceStore } from '@stores';
 
 import { Card } from './Card';
@@ -10,43 +9,36 @@ interface IMenuMain {
 }
 
 const Component = ({ selectedCategory }: IMenuMain) => {
-	const { isMobile } = useWindowDimensions();
-
-	const categories = priceStore.getCategories();
-	const sections = isMobile ? categories : [selectedCategory];
-
 	const handleGetProducts = (categoryId: string) => priceStore.getProducts(categoryId);
 
-	if (!sections) return null;
+	if (!selectedCategory) return null;
+
+	const idSection = `menu-main-${selectedCategory.category_id}`;
+	const idGrid = `menu-main-grid-${selectedCategory.category_id}`;
+	const title = selectedCategory.category_title;
+	const cards = handleGetProducts(selectedCategory.category_id);
 
 	return (
 		<div id="menu-main">
-			{sections.map((item) => {
-				const idSection = `menu-main-${item.category_id}`;
-				const idGrid = `menu-main-grid-${item.category_id}`;
-				const title = item.category_title;
-				const cards = handleGetProducts(item.category_id);
+			<div key={idSection} id={idSection} className="menu-section">
+				<div className="menu-main-header">
+					<span className="menu-main-title">{title}</span>
+					{selectedCategory.category_description ? (
+						<span>{selectedCategory.category_description}</span>
+					) : null}
+				</div>
 
-				return (
-					<div key={idSection} id={idSection} className="menu-section">
-						<div className="menu-main-header">
-							<span className="menu-main-title">{title}</span>
-							{item.category_description ? <span>{item.category_description}</span> : null}
-						</div>
-
-						<div id={idGrid} className="menu-items">
-							{cards &&
-								cards.map((card) => (
-									<Card
-										key={card.product_id}
-										productId={card.product_id}
-										categoryId={item.category_id}
-									/>
-								))}
-						</div>
-					</div>
-				);
-			})}
+				<div id={idGrid} className="menu-items">
+					{cards &&
+						cards.map((card) => (
+							<Card
+								key={card.product_id}
+								productId={card.product_id}
+								categoryId={selectedCategory.category_id}
+							/>
+						))}
+				</div>
+			</div>
 		</div>
 	);
 };
